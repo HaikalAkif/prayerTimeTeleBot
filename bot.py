4021d6a912de7bfdc1e6b9bbb17377ff0d69cee0
Elyas Asmad <elyasasmadz@gmail.com>
2023-06-07 00:51:06 +0800

    ✨ Added prayer times parser class

---
+++ bot.py
@@ -0,0 +1,51 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
+from requests.exceptions import HTTPError
+from datetime import datetime
+from prayer_times import PrayerTimes
+
+jakim_api = "https://www.e-solat.gov.my/index.php?r=esolatApi/TakwimSolat&period=today&zone=WLY01"
+
+# def get_waktu_solat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def get_waktu_solat():
+    
+    try:
+        response = requests.get(jakim_api)
+        response.raise_for_status()
+        prayer_times_dict = response.json()
+        
+        pt = PrayerTimes(prayer_times_dict['prayerTime'][0])
+        
+        # Contoh
+        print(pt.date)
+        print(pt.subuh.strftime('%I:%M %p'))
+        print(pt.zohor.strftime('%I:%M %p'))
+        print(pt.asar.strftime('%I:%M %p'))
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_chat_action('TYPING')
+    await update.message.reply_text(f'Kontol {update.effective_user.first_name}')
+
+async def get_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    
+    today = datetime.now()
+    
+    await update.message.reply_chat_action('TYPING')
+    await update.message.reply_text('Today is: ' + today.strftime('%d-%b-%Y'))
+
+app = ApplicationBuilder().token("5602722406:AAHrCpTfseKcmvK5hZibD6QmX8H3xkXgU3I").build()
+
+# Letak command sini
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("today", get_date))
+
+app.run_polling()
+
+if __name__ == "__main__":
+    get_waktu_solat()
---
+++ prayer_times.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+class PrayerTimes:
+    
+    hijrah: str
+    day: str
+    date: datetime
+    imsak: datetime
+    subuh: datetime
+    syuruk: datetime
+    zohor: datetime
+    asar: datetime
+    maghrib: datetime
+    isyak: datetime
+    
+    def __init__(self, pt_json: dict):
+        self.hijrah = pt_json['hijri']
+        self.day = pt_json['day']
+        self.date = self.__convert_to_date__(pt_json['date'])
+        self.imsak = self.__convert_to_time__(pt_json['imsak'])
+        self.subuh = self.__convert_to_time__(pt_json['fajr'])
+        self.syuruk = self.__convert_to_time__(pt_json['syuruk'])
+        self.zohor = self.__convert_to_time__(pt_json['dhuhr'])
+        self.asar = self.__convert_to_time__(pt_json['asr'])
+        self.maghrib = self.__convert_to_time__(pt_json['maghrib'])
+        self.isyak = self.__convert_to_time__(pt_json['isha'])
+    
+    def __convert_to_time__(self, time: str):
+        return datetime.strptime(time, '%H:%M:%S')
+    
+    def __convert_to_date__(self, date: str):
+        return datetime.strptime(date, '%d-%b-%Y')
